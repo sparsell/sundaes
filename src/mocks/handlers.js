8ffe75277544b1d0ba911eda472a689d0b8d5293
Sonja Parsell <sonjaparsell09@gmail.com>
2023-04-25 08:58:48 -0400

    set up handlers

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,2 +1,12 @@
 import { rest } from 'msw';
 
+export const handlers = [
+  rest.get("http://localhost:3000/scoops", (req, res, ctx) => {
+    return res(
+      ctx.json([
+        { name: 'Chocolate', imagePath: '/images/chocolate.png'}, 
+        { name: 'vanilla', imagePath: '/images/vanilla.png'}
+      ])
+    )
+  }),
+];
