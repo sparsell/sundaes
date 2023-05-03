a78583385098be25700f3b71caabaf92a768da8b
Sonja Parsell <sonjaparsell09@gmail.com>
2023-05-03 10:44:48 -0400

    adds Options

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -1,5 +1,31 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import Row from 'react-bootstrap/Row';
+import ScoopOption from './ScoopOption';
+
 export default function Options({optionType}) {
-  return <div>
-    
-  </div>
-}
+  const [items, setItems] = useState([]);
+
+  // optionType is 'scoops' or 'toppings'
+  useEffect(() => {
+    axios
+    .get(`http://localhost:3000/${optionType}`)
+    .then((response) => setItems(response.data))
+    .catch(error => {
+      //TODO add error response
+    });
+  }, [optionType]);
+
+  const ItemComponent = optionType === 'scoops' ? ScoopOption : null;
+
+  const optionItems = items.map((item) => 
+  <ItemComponent 
+    key={item.name} 
+    name={item.name} 
+    imagePath={item.imagePath}
+    />
+    );
+
+  return <Row>{optionItems}</Row>;
+
+};
