c7973abea203f3944b934e0da0f01754d9dd893b
Sonja Parsell <sonjaparsell09@gmail.com>
2023-05-03 10:45:08 -0400

    creates Scoop Options

--- src/pages/entry/ScoopOption.jsx
+++ src/pages/entry/ScoopOption.jsx
@@ -1,5 +1,16 @@
-export default function ScoopOptions() {
+import Col from 'react-bootstrap/Col';
+
+export default function ScoopOptions( {name, imagePath} ) {
   return <div>
+    <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: 'center'}}>
+
     
+      <img 
+        src={`http://localhost:3000/${imagePath}`} 
+        alt={`${name} scoop`} 
+        style={{ width: '75%' }}
+      />
+    </Col>
+
   </div>
 }
