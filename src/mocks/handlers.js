5b61dd1c36a3c35267009656858f2d72e36e1890
Sonja Parsell <sonjaparsell09@gmail.com>
2023-08-10 16:14:45 -0400

    updated localhost server

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,7 +1,7 @@
 import { rest } from 'msw';
 
 export const handlers = [
-  rest.get("http://localhost:3000/scoops", (req, res, ctx) => {
+  rest.get("http://localhost:3030/scoops", (req, res, ctx) => {
     return res(
       ctx.json([
         { name: 'Chocolate', imagePath: '/images/chocolate.png'}, 
@@ -10,7 +10,7 @@ export const handlers = [
     )
   }),
 
-  rest.get("http://localhost:3000/toppings", (req, res, ct) => {
+  rest.get("http://localhost:3030/toppings", (req, res, ctx) => {
     return res(
       ctx.json([
         { name: 'Cherries', imagePath: '/images/cherries.png' },
