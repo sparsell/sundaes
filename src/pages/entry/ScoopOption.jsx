a5a55906c8812b4b3a6ef14fad25cfcec34646f8
Sonja Parsell <sonjaparsell09@gmail.com>
2023-09-08 11:55:06 -0400

    scoop option form

--- src/pages/entry/ScoopOption.jsx
+++ src/pages/entry/ScoopOption.jsx
@@ -1,6 +1,12 @@
 import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import { useOrderDetails } from '../../contexts/OrderDetails';
 
 export default function ScoopOption({ name, imagePath }) {
+  const { updateItemCount } = useOrderDetails();
+  const handleChange = (e) => updateItemCount(name, parseInt(e.target.value), "scoops");
+
   return (
     <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: "center" }}>
       <img 
@@ -8,6 +14,23 @@ export default function ScoopOption({ name, imagePath }) {
         alt={`${name} scoop`} 
         style={{ width: '75%' }}
       />
+      <Form.Group 
+      controlId={`${name}-count`} 
+      as={Row} 
+      style={{ marginTop: "10px" }} 
+      >
+        <Form.Label column xs="6" style={{textAlign: "right"}}>
+          {name}
+        </Form.Label>
+        <Col xs="5" style={{ textAlign: "left"}}>
+            <Form.Control
+              type="number"
+              defaultValue={0}
+              onChange={handleChange}
+            />
+        </Col>
+      </Form.Group>
     </Col>
   );
 }
+
