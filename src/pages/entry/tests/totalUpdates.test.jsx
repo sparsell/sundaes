a93623ba866c27205ad9a0e33aa6a881f89879f8
Sonja Parsell <sonjaparsell09@gmail.com>
2023-09-08 11:55:18 -0400

    adds test for totals

---
+++ src/pages/entry/tests/totalUpdates.test.jsx
@@ -0,0 +1,29 @@
+  import { render, screen } from "@testing-library/react";
+import userEvent from '@testing-library/user-event';
+import Options from '../Options';
+import { OrderDetailsProvider } from "../../../contexts/OrderDetails";
+
+test("update scoop subtotal when scoops change", async () => {
+
+    const user = userEvent.setup();
+    render(<Options optionType="scoops" />, { wrapper: OrderDetailsProvider});
+    
+    // make sure total starts at $0.00
+        const scoopSubtotal = screen.getByText("Scoops total: $", {exact: false });
+        expect(scoopSubtotal).toHaveTextContent("0.00");
+
+    // update vanilla scoops to 1, and check subtotal
+        const vanillaInput = await screen.findByRole("spinbutton", { name: "Vanilla" });
+
+        await user.clear(vanillaInput);
+        await user.type(vanillaInput, "1");
+
+        expect(scoopSubtotal).toHaveTextContent("2.00");
+    // update chocolate scoops to 2, check subtotal
+        const chocolateInput = await screen.findByRole("spinbutton", { name: "Chocolate" });
+
+        await user.clear(chocolateInput);
+        await user.type(chocolateInput, "2");
+
+        expect(scoopSubtotal).toHaveTextContent("6.00");
+});
